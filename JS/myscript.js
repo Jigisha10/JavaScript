0adbd2924ba20849f131337fb653b971f7e8d9d6
Jigisha <jigisha@codetasker.com>
2024-05-16 18:57:37 +0530

    Array

--- JS/myscript.js
+++ JS/myscript.js
@@ -1,1 +1,14 @@
-document.write("External Javascript")
+document.write("External Javascript");
+function append(string1, string2){
+    return(string1+string2);
+}
+// debugger;
+var firstName="Jigisha";
+var lastName="Ahir";
+document.write(firstName,lastName);
+i = 0;
+window.onload= function(){
+    document.getElementById('changetext').onclick=function(){
+        document.getElementById("spntext").innerHTML= i++;
+    }
+}
